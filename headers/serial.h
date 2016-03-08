e4d6d555ec5034ba8652d8ba248f2d5384ad3d70
Alex Meling <alexmeling@hotmail.com>
2016-03-08 11:32:40 -0800

    new branch serial, class prototype added

---
+++ headers/serial.h
@@ -0,0 +1,21 @@
+/**file serial.h
+ * @brief Class prototype for the serial communication driver
+ * @author Alex Velásquez Meling
+ * @bugs No Known bugs
+ */ 
+
+
+#ifndef SERIAL_H
+#define SERIAL_H
+
+/**
+ * @class serial
+ * @brief this contains the attributs and methods for the serial communication driver
+ */
+
+
+class serial{
+
+};
+
+#endif // SERIAL_H
---
+++ sources/serial.cpp
@@ -0,0 +1,17 @@
+/* @file serial.cpp
+ * @brief serial communication driver
+ * these functions are provided so that the serial communication driver will manage the serial interface without complaining.
+ *
+ * @author Alex Velásquez Meling
+ * @bug no bugs known
+ */
+
+#include <stdio.h>
+#include <avr/io.h>
+#include <util/delay.h>
+#include "../headers/serial.h"
+
+using namespace std;
+
+
+
